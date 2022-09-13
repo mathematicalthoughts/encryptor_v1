d6e57e502d336da16fb40d7b43751b71c0130025
Jonathan Alejandro Muñoz <munozmartinez.ja@gmail.com>
2022-09-13 20:37:43 -0300

    Función y Algoritmo de Encritación Finalizados

--- functions/functions.js
+++ functions/functions.js
@@ -1,56 +1,35 @@
-//¡Bienvenidos y Bienvenidas a nuestro primer desafío! 
-//
-//Durante estas cuatro semanas, vamos a trabajar en una aplicación que encripta textos, así podrás intercambiar mensajes secretos con otras personas que sepan el secreto de la encriptación utilizada.
-//
-//Las "llaves" de encriptación que utilizaremos son las siguientes:
-//
-//`La letra "e" es convertida para "enter"`
-//`La letra "i" es convertida para "imes"`
-//`La letra "a" es convertida para "ai"`
-//`La letra "o" es convertida para "ober"`
-//`La letra "u" es convertida para "ufat"`
-//
-//**Requisitos:**
-//- Debe funcionar solo con letras minúsculas
-//- No deben ser utilizados letras con acentos ni caracteres especiales
-//- Debe ser posible convertir una palabra para la versión encriptada también devolver una palabra encriptada para su versión original. 
-//
-//Por ejemplo:
-//`"gato" => "gaitober"`
-//`gaitober" => "gato"`
-//
-//- La página debe tener campos para 
-//inserción del texto que será encriptado o desencriptado, y el usuario debe poder escoger entre as dos opciones.
-//- El resultado debe ser mostrado en la pantalla.
-//
-//**Extras:**
-//- Un botón que copie el texto encriptado/desencriptado para la sección de transferencia, o sea que tenga la misma funcionalidad del `ctrl+C` o de la opción "copiar" del menú de las aplicaciones.
-//
-//Tenemos un periodo de tiempo de cuatro semanas para desarrollar el proyecto y vamos a trabajar con el sistema ágil de desarrollo, utilizando el Trello de la siguiente forma: 
-//
-//1. La columna **Listos para iniciar** presenta las tarjetas con elementos que aun no fueron desarrollados.
-//2. En la columna **En Desarrollo** estarán los elementos que estés desarrollando en el momento. Al iniciar una tarea, podrás mover la tarjeta que contiene dicha tarea para esta columna.
-//3. En la columna **Pausado** estarán los elementos que comenzaste a desarrollar, pero necesitaste parar por algún motivo.
-//4. Por fin, en la columna **Concluido** estarán los elementos ya concluidos.
-//
-//El Trello es una herramienta de uso individual para que puedas controlar el progreso de tus actividades, pero no será evaluada.
-//
-// Buen proyecto!
+//Encryptor Function
 
-let in_str = 'murcielago'
-
-function wordEncryptor(word) {
-    let vocals_list = ['a','e','i','o','u']
-    let input_str = word;
-    let input_arr = input_str.split('');
-    console.log(input_arr)
-    for (i=0; i < input_arr.length; i++) {
-        console.log(input_arr[i] + ' index = '+ i)
-        
-    }
+function wordEncryptor_v1(word){
     
+    let vocals_list = ['a','e','i','o','u']
+    let subtitutions = ['ai','enter','imes','ober','ufat']
+    let input_str = word
 
+    let input_arr = input_str.split('');
+    //console.log(input_arr)
 
+    //First Comparative For Cicle
+    for (i=0; i < input_arr.length; i++) {
+        //console.log('Comparing *' + input_arr[i] + '* with vocals list')
+        //Second For Cicle - Identify and Swap
+        for(j=0; j < vocals_list.length; j++){
+            if (input_arr[i] === vocals_list[j]) {
+                //console.log('Itering in the character = ' + vocals_list[j] + ' > index = '+ j)
+                //console.log('Match: Yes')
+
+                //Swaping
+                input_arr[i] = subtitutions[j]
+                //console.log(input_arr);
+
+         //if (input_arr[i] !== vocals_list[j]){
+            // console.log('Match: No')
+         //}  
+            }
+        }
+    }
+    let output_str = input_arr.join('')
+    //console.log('Encrypted text -------> ' + output_str)
+    word = output_str;
+    return word;
 }
-
-wordEncryptor(in_str)
